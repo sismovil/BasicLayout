71cbc4e5bfab72f45650b6e419bf42e93462f7f0
cfigmart <cfigmart@unicauca.edu.co>
2025-09-02 16:39:30 -0500

    Added images and AlignYourBodyElement

--- app/src/main/java/edu/unicauca/aplimovil/basiclayout/MainActivity.kt
+++ app/src/main/java/edu/unicauca/aplimovil/basiclayout/MainActivity.kt
@@ -5,10 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
@@ -18,7 +22,11 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -67,4 +75,32 @@ fun SearchBar(
 fun SearchBarPreview(){
     SearchBar()
 }
+
+@Composable
+fun AlignYourBodyElement(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+        Image(
+            painter = painterResource(R.drawable.ab1_inversions),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = modifier
+                .size(80.dp)
+                .clip(shape = CircleShape)
+        )
+        Text(text = stringResource(R.string.ab1_inversions))
+    }
+}
+
+
+@Preview(locale = "es")
+@Composable
+fun AlignYourBodyElementPreview(){
+    AlignYourBodyElement()
+}
+
 
